5e9c988f0d3541b618e05f6ecaaf25062af29a53
chytran <chytran@users.noreply.github.com>
2021-08-29 23:05:01 -0700

    Setting up styles

---
+++ includes/functions.inc.php
@@ -0,0 +1,23 @@
+<?php 
+
+function noMatchCredit($creditNumber, $creditNumberRepeat) {
+    $result;
+    if ($creditNumber !== $creditNumberRepeat) {
+        $result = true;
+    }
+    else {
+        $result = false;
+    }
+    return $result;
+}
+
+function wrongPrice($price) {
+    $result;
+    if (!is_numeric($price)) {
+        $result = false;
+    }
+    else {
+        $result = true;
+    }
+    return $result;
+}
--- index.php
+++ index.php
@@ -15,73 +15,77 @@
 </head>
 <body>
     <section class="main__container">
-        <?php
-            if ($_GET["error"] == "invalidPrice") {
-                echo "<p color="red">Must Enter a valid price.</p>";
-            }
-            else if ($_GET["error"]  == "noMatchCredit") {
-                echo  "<p color="red">Credit card numbers do not match.</p>";
-            }
-        ?>
-        <form action="POST" action="display.php" class="form">
-            <h1 class="order__title">Order Form</h1>
+        <div class="container bg">
+            <?php
+                if (isset($_GET["error"])) {
+                    if ($_GET["error"] == "invalidPrice") {
+                        echo "<p class='error'>Must Enter a valid price.</p>";
+                    }
+                    else if ($_GET["error"]  == "noMatchCredit") {
+                        echo "<p class='error'>Credit card numbers do not match.</p>";
+                    }
+                }
+            ?>
+            <form action="POST" action="display.php" class="form">
+                <h1 class="order__title">Order Form</h1>
 
-            <div class="product__container container__organizer">
-                <h2 class="product__name">Product:</h2>
-                <input name="productName" type="text" class="product__text">
-            </div>
+                <div class="product__container container__organizer">
+                    <h2 class="product__name">Product:</h2>
+                    <input name="productName" type="text" class="product__text">
+                </div>
 
-            <div class="quantity__container container__organizer">
-                <h2 class="quantity__name">Quantity:</h2>
-                <select name="quantity" id="quantity">
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                    <option value="6">6</option>
-                </select>
-            </div>
+                <div class="quantity__container container__organizer">
+                    <h2 class="quantity__name">Quantity:</h2>
+                    <select name="quantity" id="quantity">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                    </select>
+                </div>
 
-            <div class="unit__container">
-                <h2 class="unit__name">Unit Price:</h2>
-                <input type="text" name="price" class="unit__text">
-            </div>
+                <div class="unit__container">
+                    <h2 class="unit__name">Unit Price:</h2>
+                    <input type="text" name="price" class="unit__text">
+                </div>
 
-            <div class="name__container container__organizer">
-                <h2 class="name__name">Name:</h2>
-                <input type="text" name="name" class="name__text">
-            </div>
+                <div class="name__container container__organizer">
+                    <h2 class="name__name">Name:</h2>
+                    <input type="text" name="name" class="name__text">
+                </div>
 
-            <div class="shipping__container container__organizer">
-                <h2 class="shipping__name">Shipping Address:</h2>
-                <input type="text" name="shipping" class="shipping__text">
-            </div>
+                <div class="shipping__container container__organizer">
+                    <h2 class="shipping__name">Shipping Address:</h2>
+                    <input type="text" name="shipping" class="shipping__text">
+                </div>
 
-            <div class="credit__container container__organizer">
-                <h2 class="credit__name">Credit Card:</h2>
-                <fieldset id="credit1">
-                    <input type="radio" value="value1" name="credit" class="credit__option">
-                    <input type="radio" value="value2" name="credit" class="credit__option">
-                    <input type="radio" value="value3" name="credit" class="credit__option">
-                    <input type="radio" value="value4" name="credit" class="credit__option">
-                </fieldset>
-            </div>
+                <div class="credit__container container__organizer">
+                    <h2 class="credit__name">Credit Card:</h2>
+                    <fieldset id="credit1">
+                        <input type="radio" value="value1" name="credit" class="credit__option">
+                        <input type="radio" value="value2" name="credit" class="credit__option">
+                        <input type="radio" value="value3" name="credit" class="credit__option">
+                        <input type="radio" value="value4" name="credit" class="credit__option">
+                    </fieldset>
+                </div>
 
-            <div class="credit__number__container container__organizer">
-                <h2 class="credit__number__name">Credit Card Number:</h2>
-                <input type="password" class="credit__number__text" name="creditNumber">
-            </div>
+                <div class="credit__number__container container__organizer">
+                    <h2 class="credit__number__name">Credit Card Number:</h2>
+                    <input type="password" class="credit__number__text" name="creditNumber">
+                </div>
 
-            <div class="repeat__credit__number__container container__organizer">
-                <h2 class="repeat__credit__number__name">Credit Card Number:</h2>
-                <input type="password" class="repeat__credit__number__text" name="repeatCredit">
-            </div>
+                <div class="repeat__credit__number__container container__organizer">
+                    <h2 class="repeat__credit__number__name">Credit Card Number:</h2>
+                    <input type="password" class="repeat__credit__number__text" name="repeatCredit">
+                </div>
 
-            <hr class="divider">
+                <hr class="divider">
 
-            <button class="button" method="POST" name="submit">Submit Order</button>
-        </form>
+                <button class="button" method="POST" name="submit">Submit Order</button>
+            </form>
+        </div>
     </section>
 </body>
 </html>
